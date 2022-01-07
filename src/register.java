2ee09cf6703658eb62ff6d3f40891e76b7f4f806
John Nixon <69578073+JohnNixon6972@users.noreply.github.com>
2022-01-07 22:26:37 +0530

    Added update register component

--- src/register.java
+++ src/register.java
@@ -5,7 +5,7 @@ public class register {
     public static void main(String args[]){
         Scanner scan = new Scanner(System.in);
         System.out.println("------------------------------ Registration Page --------------------");
-        String f_name,l_name,address,city,password;
+        String f_name,l_name,address,city,password,dob,adhaar_card_no,constituency,state;
         long mob_num;
         System.out.print("First Name :: ");
         f_name = scan.nextLine();
@@ -19,7 +19,20 @@ public class register {
         city = scan.nextLine();
         System.out.print("Mobile Number :: ");
         mob_num = scan.nextLong();
-        
+        scan.nextLine();
+        System.out.print("Date of Brith (YYYY-MM-DD):: ");
+        dob = scan.nextLine();
+        System.out.print("Aadhar Card Number :: ");
+        adhaar_card_no = scan.nextLine();
+        System.out.print("Constituency :: ");
+        constituency = scan.nextLine();
+        System.out.print("State :: ");
+        state = scan.nextLine();
+
+        if(adhaar_card_no.length() != 12){
+            System.out.println("Invalid Aadhar number :: ");
+            System.exit(0);
+        }
 
         int min_val = 100000;
         int max_val = 999999;
@@ -31,17 +44,16 @@ public class register {
         // System.out.print(f_name+" "+l_name+" "+address+" "+city+" "+mob_num);
         
         scan.close();
-        String database_user = "Hrutuja Patnekar";
-        String database_password = "root";
-
+        String database_user = "john";
+        String database_password = "password";
 
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection con = DriverManager.getConnection(
                     "jdbc:mysql://localhost:3306/online_voting_system", database_user, database_password);
             Statement stmt = con.createStatement();
-            String query = String.format("insert into users values(%d,\"%s\",\"%s\",\"%s\",\"%s\",\"%s\",%o);",id,password,f_name,l_name,address,city,mob_num);
-            System.out.println(query);
+            String query = String.format("insert into users values(%d,\"%s\",\"%s\",\"%s\",\"%s\",\"%s\",%o,\"%s\",\"%s\",\"%s\",\"%s\");",id,password,f_name,l_name,address,city,mob_num,dob,adhaar_card_no,constituency,state);
+            // System.out.println(query);
             stmt.executeUpdate(query);
             System.out.println("User Created Sucessfully!!");
 
